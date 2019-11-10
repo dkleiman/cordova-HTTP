e4469944e15cdd679fb0b54da8f8a15df17c2e4d
David Kleiman <dave.kleiman2@gmail.com>
2019-11-10 11:17:23 -0500

    Keep trimming fat

--- www/cordovaHTTP.js
+++ www/cordovaHTTP.js
@@ -4,21 +4,8 @@
 
 var exec = require('cordova/exec');
 
-function mergeHeaders(globalHeaders, localHeaders) {
-    var globalKeys = Object.keys(globalHeaders);
-    var key;
-    for (var i = 0; i < globalKeys.length; i++) {
-        key = globalKeys[i];
-        if (!localHeaders.hasOwnProperty(key)) {
-            localHeaders[key] = globalHeaders[key];
-        }
-    }
-    return localHeaders;
-}
-
 var http = {
     getChunked: function(url, params, headers, success, failure) {
-        headers = mergeHeaders(this.headers, headers);
         return exec(success, failure, "CordovaHttpPluginChunks", "getChunked", [url, params, headers]);
     },
 };
